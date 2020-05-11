1b06ceacceb7a332f28d53639bbd22d13dd1885b
Brandon Maroni <brandon-maroni@lambdastudents.com>
2020-05-10 22:03:45 -0600

    finished task 2

--- index.js
+++ index.js
@@ -62,11 +62,13 @@ function counter2() {
 
 Write a function called `inning` that generates a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
+function inning(){
 
-    /*Code Here*/
+    let score = Math.floor(Math.random() * 3);
+    return score
 
 }
+console.log(inning())
 
 /* Task 3: finalScore()
 
@@ -82,11 +84,17 @@ finalScore(inning, 9) might return:
 
 */ 
 
-function finalScore(/*code Here*/){
+function finalScore(cb, numOfIns){
 
-  /*Code Here*/
+  let homeScore = cb();
+  let awayScore = cb();
 
+  return {
+    "Home": homeScore,
+    "Away": awayScore
+  }
 }
+console.log(finalScore(inning, 1))
 
 /* Task 4: 
 
